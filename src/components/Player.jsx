701b2ed16b60a3e9b03d58972f0b98c2e0309f3a
mrw007 <mr.wahib@gmail.com>
2026-05-13 11:07:27 +0100

    add Player section

---
+++ src/components/Player.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+export default function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    setIsEditing((editing) => !isEditing);
+  }
+
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
+
+  return (
+    <li>
+      <span className="player">
+        {isEditing ? (
+          <input
+            type="text"
+            value={playerName}
+            onChange={handleChange}
+            required
+          />
+        ) : (
+          <span className="player-name">{playerName}</span>
+        )}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
+    </li>
+  );
+}
